ff67b252a38161c35e32914a91c6022556822333
martinavukojevic <147094810+martinavukojevic@users.noreply.github.com>
2023-10-19 13:28:20 +0200

    Add files via upload

--- martina-vukojevic_vjezba2zad1.cpp
+++ martina-vukojevic_vjezba2zad1.cpp
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
-void funkcija(int* niz, int &min, int &max, int n) {
-	int i;
+void funkcija(int* niz, int& min, int& max, int n) {
+    int i;
 
-	for (i = 1; i < n; i++) {
-		if (niz[i] > min) {
-			min = niz[i];
-		}
-		if (niz[i] < max) {
-			max = niz[i];
-		}
-	}
+    for (i = 0; i < n; i++) {
+        if (niz[i] < min) {
+            min = niz[i];
+        }
+        if (niz[i] > max) {
+            max = niz[i];
+        }
+    }
 }
 
 int main() {
-	int niz[] = { 3, 5, 2, 7, 1, 8, 0 };
-	int n = 7;
+    int niz[] = { 3, 5, 2, 7, 1, 8, 0 };
+    int n = 7;
 
-	int min = niz[0];
-	int max = niz[0];
+    int min = niz[0];
+    int max = niz[0];
 
-	funkcija(niz, min, max, n);
-	return 0;
+    funkcija(niz, min, max, n);
+
+    printf("Najmanji broj: %d\n", min);
+    printf("Najveći broj: %d\n", max);
+
+    return 0;
 }
