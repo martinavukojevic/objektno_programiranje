bba4fdbeb4b6ae86d202204ee5cac5a121c03e17
martinavukojevic <147094810+martinavukojevic@users.noreply.github.com>
2023-11-30 14:39:09 +0100

    Add files via upload

---
+++ vj07zad01.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+class Hrana {
+private:
+	static int brojac;
+
+public:
+	Hrana() {
+		brojac++;
+	}
+
+	~Hrana() {
+		brojac--;
+	}
+
+	static int dohvatiBrojac() {
+		return brojac;
+	}
+
+	static void postaviBrojac(int vrijednost) {
+		brojac = vrijednost;
+	}
+
+	static void povecajBrojac() {
+		brojac++;
+	}
+
+	static void smanjiBrojac() {
+		brojac--;
+	}
+
+	static void ispisiBrojac() {
+		std::cout << "Brojac: " << brojac << std::endl;
+	}
+};
+
+int Hrana::brojac = 0;
+
+int main() {
+	Hrana hrana1;
+	Hrana hrana2;
+	Hrana hrana3;
+
+	Hrana::ispisiBrojac();  
+
+	Hrana::povecajBrojac();
+	Hrana::ispisiBrojac();  
+
+	Hrana::smanjiBrojac();
+	Hrana::ispisiBrojac();  
+
+	Hrana::postaviBrojac(10);
+	Hrana::ispisiBrojac();  
+	return 0;
+}
+
