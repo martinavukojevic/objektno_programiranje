1d5b8cd40a706eb4fa7498ed63b9ea31777c7033
martinavukojevic <147094810+martinavukojevic@users.noreply.github.com>
2023-12-21 14:42:51 +0100

    Add files via upload

---
+++ vjezba10/1vjezba10.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <string>
+
+using namespace std;
+
+void promijeniRijeci(const string& imeDatoteke) {
+	ifstream datoteka(imeDatoteke);
+	if (!datoteka.is_open()) {
+		cerr << "Nemoguće otvoriti datoteku: " << imeDatoteke << endl;
+		return;
+	}
+
+	istream_iterator<string> it(datoteka), kraj;
+	vector<string> rijeci(it, kraj);
+
+	vector<int> pozicije;
+	auto itPronadi = find(rijeci.begin(), rijeci.end(), "odredena_rijec");
+	while (itPronadi != rijeci.end()) {
+		pozicije.push_back(distance(rijeci.begin(), itPronadi));
+		itPronadi = find(next(itPronadi), rijeci.end(), "odredena_rijec");
+	}
+
+	// 3. Izbaci sve stringove koji sadrže određeni podstring koristeći remove_if i erase.
+	rijeci.erase(remove_if(rijeci.begin(), rijeci.end(),
+		[](const string& s) { return s.find("odredeni_podstring") != string::npos; }),
+		rijeci.end());
+
+	// 4. Koristi transform, promijeni sva slova u stringovima u velika slova.
+	transform(rijeci.begin(), rijeci.end(), rijeci.begin(),
+		[](string s) {
+		transform(s.begin(), s.end(), s.begin(), ::toupper);
+		return s;
+	});
+
+	// 5. Ispiši stringove iz vektora koristeći copy.
+	copy(rijeci.begin(), rijeci.end(), ostream_iterator<string>(cout, " "));
+	cout << endl;
+}
+
+int main() {
+	// Poziv funkcije za riječi
+	promijeniRijeci("words.txt");
+
+	return 0;
+}
+
---
+++ vjezba10/2vjezba10.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <cmath>
+
+using namespace std;
+
+// Struktura koja predstavlja točku
+struct Tocka {
+	double x, y;
+
+	Tocka(double x_val, double y_val) : x(x_val), y(y_val) {}
+};
+
+void obradiTocke(const string& imeDatoteke) {
+	// 1. Napuni vektor točaka koristeći istream iterator.
+	ifstream datoteka(imeDatoteke);
+	if (!datoteka.is_open()) {
+		cerr << "Nemoguće otvoriti datoteku: " << imeDatoteke << endl;
+		return;
+	}
+
+	istream_iterator<Tocka> it(datoteka), kraj;
+	vector<Tocka> tocke(it, kraj);
+
+	// 2. Sortiraj točke prema udaljenosti od ishodišta koristeći sort.
+	sort(tocke.begin(), tocke.end(),
+		[](const Tocka& t1, const Tocka& t2) {
+		return sqrt(t1.x * t1.x + t1.y * t1.y) < sqrt(t2.x * t2.x + t2.y * t2.y);
+	});
+
+	// 3. Prebroji koliko točaka leži unutar određenog kruga s centrom u ishodištu koristeći count if.
+	int brojTocakaUnutarKruga = count_if(tocke.begin(), tocke.end(),
+		[](const Tocka& t) {
+		return sqrt(t.x * t.x + t.y * t.y) < 5.0; // Promijenite radijus prema potrebi
+	});
+
+	// 4. Zamijeni sve točke koje su točno na određenoj udaljenosti od ishodišta s novom točkom koristeći replace if.
+	replace_if(tocke.begin(), tocke.end(),
+		[](const Tocka& t) {
+		return abs(sqrt(t.x * t.x + t.y * t.y) - 3.0) < 0.001; // Promijenite udaljenost prema potrebi
+	},
+		Tocka(10.0, 10.0)); // Novi podaci za zamjenu
+
+// 5. Ispiši točke iz vektora u obrnutom redoslijedu koristeći reverse i copy.
+	reverse(tocke.begin(), tocke.end());
+	copy(tocke.begin(), tocke.end(), ostream_iterator<Tocka>(cout, "\n"));
+}
+
+int main() {
+	// Poziv funkcije za točke
+	obradiTocke("points.txt");
+
+	return 0;
+}
