9347565356659763cf9da847e2046f9d507bbc5f
martinavukojevic <147094810+martinavukojevic@users.noreply.github.com>
2024-01-11 18:04:07 +0100

    1vjezba10.cpp

--- vjezba10/1vjezba10.cpp
+++ vjezba10/1vjezba10.cpp
@@ -24,25 +24,25 @@ void promijeniRijeci(const string& imeDatoteke) {
 		itPronadi = find(next(itPronadi), rijeci.end(), "odredena_rijec");
 	}
 
-	// 3. Izbaci sve stringove koji sadrže određeni podstring koristeći remove_if i erase.
+	
 	rijeci.erase(remove_if(rijeci.begin(), rijeci.end(),
 		[](const string& s) { return s.find("odredeni_podstring") != string::npos; }),
 		rijeci.end());
 
-	// 4. Koristi transform, promijeni sva slova u stringovima u velika slova.
+	
 	transform(rijeci.begin(), rijeci.end(), rijeci.begin(),
 		[](string s) {
 		transform(s.begin(), s.end(), s.begin(), ::toupper);
 		return s;
 	});
 
-	// 5. Ispiši stringove iz vektora koristeći copy.
+	
 	copy(rijeci.begin(), rijeci.end(), ostream_iterator<string>(cout, " "));
 	cout << endl;
 }
 
 int main() {
-	// Poziv funkcije za riječi
+	
 	promijeniRijeci("words.txt");
 
 	return 0;
